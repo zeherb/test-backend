d27d524fc75cabeceae226e7cb24ad53de3acf18
zeherb <bouzid.zeher@gmail.com>
2021-10-06 13:42:54 +0100

    refactoring

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -1,18 +1,16 @@
 const Vote = require('../models/votesSchema')
 const Subject = require('../models/subjectSchema')
 const User = require('../models/userSchema')
-const { findByIdAndUpdate } = require('../models/userSchema')
 
 exports.create = async (req, res) => {
     try {
         const user = await User.findById(req.body.owner)
-        console.log(user);
         if (user.voteNumber > 0) {
             const vote = await Vote.create(req.body)
             // affect this vote to the subject 
             const subject = await Subject.findByIdAndUpdate(vote.subject, { $push: { votes: vote._id } }, { new: true })
             // update users vote number
-            const UpdatedUser = await User.findByIdAndUpdate(vote.owner, { voteNumber: user.voteNumber - 1 })
+            const UpdatedUser = await User.findByIdAndUpdate(vote.owner, { "$inc": { voteNumber: -1 } })
             res.status(200).json({ message: 'Vote created successfully' })
         } else {
             res.status(400).json({ message: 'You have reached your voting limit' })
